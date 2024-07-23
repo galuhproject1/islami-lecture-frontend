ffb9d507130ac30c2dd282deef3a92b441761b85
galuhindico1 <tech_dev1@indicocompany.com>
2024-07-23 20:42:10 +0700

    Galuh | setup auth repositoy &  env

--- src/repository/auth.ts
+++ src/repository/auth.ts
@@ -1,16 +1,11 @@
+import { api } from "../libs/api";
 
 const baseUrl = import.meta.env.VITE_API;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const register = async (data: any) => {
-    const response = await fetch(`${baseUrl}/auth/register`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-    });
-    return response.json();
+    const response = await api.post(`${baseUrl}/auth/register`, data);
+    return response;
 };
 
 export const authRepository = {
