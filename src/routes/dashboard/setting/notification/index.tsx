7902936bd16ccd88c80573f93bdf899051f4373e
galuhproject1 <galuhproject1@gmail.com>
2024-08-28 23:01:15 +0700

    add: function read notif

--- src/routes/dashboard/setting/notification/index.tsx
+++ src/routes/dashboard/setting/notification/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, IconButton, Typography } from "@mui/material";
 import SettingPage from "..";
 import { MdOutlineMarkEmailUnread } from "react-icons/md";
 import { useEffect, useState } from "react";
@@ -6,11 +6,16 @@ import { getNotification } from "../../../../api/notification/getNotification";
 import { NotificationType } from "../../../../libs/Types/notifications";
 import { FaRegCheckSquare } from "react-icons/fa";
 import NothingNotif from "../../../../components/reusable/EmptyState/NothingNotif";
+import { formatDistanceToNowStrict, isToday, isYesterday } from "date-fns";
+import { IoIosArrowRoundBack } from "react-icons/io";
+import api from "../../../../libs/api";
 
 const NotificationSetting = () => {
   const [dataNotifications, setDataNotifications] = useState<
     NotificationType[]
   >([]);
+  const [detailNotif, setDetailNotif] = useState<boolean>(false);
+
   useEffect(() => {
     const fetchNotifications = async () => {
       const { data } = await getNotification();
@@ -19,12 +24,42 @@ const NotificationSetting = () => {
     };
     fetchNotifications();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [detailNotif]);
+
+  function formatNotificationDate(createdAt: string): string {
+    const date = new Date(createdAt);
+
+    if (isToday(date)) {
+      const hoursDifference = formatDistanceToNowStrict(date, { unit: "hour" });
+      return `${hoursDifference} ago`;
+    }
+
+    if (isYesterday(date)) {
+      return "1 day ago";
+    }
+
+    return formatDistanceToNowStrict(date);
+  }
+
+  const updateNotifications = async (id: string) => {
+    const data = {
+      notification_ids: id,
+    };
+    try {
+      const response = await api.post("/user/notifications/read", data);
+      if (response.status === 200) {
+        setDetailNotif(true);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <SettingPage>
       <Box
         sx={{
-          display: "flex",
+          display: detailNotif ? "none" : "flex",
           flexDirection: "column",
           width: "100%",
           height: "100%",
@@ -36,6 +71,7 @@ const NotificationSetting = () => {
       >
         <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
           <Button
+            disabled
             variant="text"
             sx={{
               gap: 1,
@@ -71,6 +107,7 @@ const NotificationSetting = () => {
                 backgroundColor: "#E8E8E8",
               },
             }}
+            onClick={() => updateNotifications(notification.id)}
           >
             <Box sx={{ display: "flex", gap: 2 }}>
               <Box
@@ -128,7 +165,8 @@ const NotificationSetting = () => {
                   fontFamily: "Mulish",
                 }}
               >
-                {notification?.created_at}
+                {notification?.created_at &&
+                  formatNotificationDate(notification?.created_at)}
               </Typography>
               <Box
                 sx={{
@@ -136,12 +174,21 @@ const NotificationSetting = () => {
                   height: "8px",
                   borderRadius: "50%",
                   backgroundColor: "#DB5962",
+                  display: notification?.read_at === null ? "block" : "none",
                 }}
               ></Box>
             </Box>
           </Box>
         ))}
       </Box>
+      {detailNotif && (
+        <Box sx={{ gap: 2, p: 4 }}>
+          <IconButton onClick={() => setDetailNotif(false)}>
+            <IoIosArrowRoundBack className="text-primary" size={24} />
+          </IconButton>
+          <Typography>Detail Notification</Typography>
+        </Box>
+      )}
     </SettingPage>
   );
 };
